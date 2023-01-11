600f5fe541668f417c67fc9349bd34a68ae713d4
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2023-01-11 21:20:21 +0600

    load image by client to host imgbb and sent ui then catch and load authentication

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -1,9 +1,14 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  // COnfig code goes here
-}
+  apiKey: "AIzaSyCToesawKVgrpY9PUPuExwhQx1fM0LqNQw",
+  authDomain: "aircnc-fa180.firebaseapp.com",
+  projectId: "aircnc-fa180",
+  storageBucket: "aircnc-fa180.appspot.com",
+  messagingSenderId: "874247667242",
+  appId: "1:874247667242:web:d7f63786af5885e8c4cd53",
+};
 
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
-export default app
+export default app;
